692d7dcd9ca840184612c1c444fc2a0dba62cbb7
Brandan Sayarath <brandan@brandan.tech>
2020-12-18 18:22:57 -0800

    memoize fetchUser function

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,11 +10,15 @@ export const fetchPosts = () => async (dispatch) => {
   });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({
     type: "FETCH_USER",
     payload: response.data,
   });
-};
+});
